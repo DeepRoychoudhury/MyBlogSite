795c6d10f8794474fa5be13356f9f85db353028d
DeepRoychoudhury <deeproydrc@gmail.com>
2020-11-23 15:31:14 +0000

    First commit of blog

---
+++ external.js
@@ -0,0 +1,19 @@
+function About(){
+	document.getElementById("aboutDeep").innerHTML="<b>Experienced Software Developer (Java) | Pursuing MSc in Cloud Computing(Software-as-a-Service) | Java | Ruby on Rails</b> <br/><br/>Hi there !!! My name is Deep Roychoudhury, an experienced(4 years & 8 months), Senior Software Engineer, with a demonstrated history of working in the information technology and services industry.I am experienced in application development and in application support as well. <br/><br/> My key skills include Java, Spring MVC, Spring Boot, Hibernate, and Ruby on Rails. I am well versed with Object-Oriented Programming (OOPs) and have a knack for problem-solving. I also have skills from my prior experience in Incident Management, Github, C++, C, and Selenium Webdriver. <br/><br/> Currently, I am pursuing a Master's degree in Cloud Computing, which is providing me an opportunity to learn new cloud technologies. My specialization is in cloud application development (Software-as-a-Service) and worked on course projects with various CSPs (AWS, Azure, GCP, Heroku). <br/><br/> Passionate about learning new technologies and have the ability to grasp quckly about the subject matter. ";
+}
+
+function removeAbout(){
+	document.getElementById("aboutDeep").innerHTML="";
+}
+
+function LinkedinLink(){
+	window.location.href="https://www.linkedin.com/in/deeproychoudhury/";
+}
+
+function FacebookLink(){
+	window.location.href="https://www.facebook.com/deep.roychoudhury";
+}
+
+function InstagramLink(){
+	window.location.href="https://www.instagram.com/deeps_story/";
+}
