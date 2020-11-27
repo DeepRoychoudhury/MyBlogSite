a676f66e3c2167c86875d586171892bac9bfdb1a
DeepRoychoudhury <deeproydrc@gmail.com>
2020-11-27 21:05:10 +0000

    Modifications for Schooling.

--- external.js
+++ external.js
@@ -17,3 +17,29 @@ function FacebookLink(){
 function InstagramLink(){
 	window.location.href="https://www.instagram.com/deeps_story/";
 }
+
+function GithubLink(){
+	window.location.href="https://github.com/DeepRoychoudhury";
+}
+
+function School(){
+	document.getElementById("aboutDeepsHistory").innerHTML="<b>My School History</b> <br/><br/>So... you want to Know where did I went for schooling. Hmmmmm !!! Let me take you to my Kindergarden school where I found my passion for sports. I went to Kindergarden in a place named ARC Charbatia, which is situated in Odisha, India during the year 1998 - 1999. ARC Charbatia is a kind of remote place in Odisha, but has a great historic resemblance of India. Though, many people of India are still unaware about the place due to its remote location. I did both my Nursery and Preparatory or prep learning from that school and Yes I was very good at athletics at that time.<br/><br/>Later on, I started my primary schooling from St. Xaviers High School, Cuttack. Cuttack is famous for a lot of things, so it won't be fair to describe the place here. The school was next to Barabati Cricket Stadium, one of the most important place in Odisha for cricket lovers, and the teaching was amazing. Inspite of having such a great time, I did my studies there till Class or Standard 'III' and later on shifted to another school due to a lot of burden in travelling and time. <br/><br/> I did the rest of my schooling from Kendriya Vidyalaya Charbatia. It was really a fun to be at that school and the teachers were amazing. I found my passion for Cricket and had played regional level cricket from school. After, class 10th I took Computer Science as my specialization course and successfully cleared all the exams. I completed my schooling in the year 2011 and there you go. This is my schooling history.";
+}
+
+function removeFooter(){
+	document.getElementById("aboutDeepsHistory").innerHTML="";
+}
+
+function College(){
+	document.getElementById("aboutDeepsHistory").innerHTML="";
+}
+
+
+function Office(){
+	document.getElementById("aboutDeepsHistory").innerHTML="";
+}
+
+
+function Hobby(){
+	document.getElementById("aboutDeepsHistory").innerHTML="";
+}
